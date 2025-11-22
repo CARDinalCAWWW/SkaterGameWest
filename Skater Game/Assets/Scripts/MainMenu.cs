3fe6dd98469ca195517627b271c1ed777663f4cd
CARDinalCAWWW <93416662+CARDinalCAWWW@users.noreply.github.com>
2025-11-22 17:42:25 -0500

    Fix to Menu
    Menu wasn't quitable lol

--- Skater Game/Assets/Scripts/MainMenu.cs
+++ Skater Game/Assets/Scripts/MainMenu.cs
@@ -3,7 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-[SerializeField] GameObject playButton;
+    [SerializeField] GameObject carneyButton;
+    [SerializeField] GameObject playButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,5 +21,19 @@ public class MainMenu : MonoBehaviour
     public void BeginGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+        public void QuitGame()
+    {
+        if (Application.isEditor)
+        {
+            // Inside Unity Editor
+            Debug.Log("Would quit the game if it were a build.");
+        }
+        else
+        {
+            // In a build
+            Application.Quit();
+        }
     }
 }
